dfadb9a95ddf2cebed9868a7f987891ba743cdba
juniorperez2026 <jperezdextratekia.com>
2026-04-29 16:18:21 -0500

    Fix base path for assets

--- src/data/deudorHeaderMock.ts
+++
@@ -1,9 +0,0 @@
-import type { DeudorHeaderInfo } from '../types';
-
-export const deudorHeaderMock: DeudorHeaderInfo = {
-  grupo: 'CLARO CORPORATIVO',
-  cartera: 'CORP_TOP_INT_FEB-26',
-  cliente: 'CLARO CORPORATIVO',
-  deudor: 'S.A.A. INVERSIONES CENTENARIO',
-  rucDni: '20101045995',
-};
